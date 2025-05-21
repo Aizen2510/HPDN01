326c3df326e05450040ccee327502d13f983da6c
Aizen2510 <vu09097@gmail.com>
2025-05-21 16:11:38 +0700

    up2

---
+++ project/src/components/Menu.jsx
@@ -0,0 +1,21 @@
+
+const Menu = () => {
+    return (
+        <>
+            <div className="flex flex-row items-center justify-between">
+                <div>
+                    Menu
+                </div>
+                <div className="flex flex-row justify-center">
+                    tinh nang
+                </div>
+                <div>
+                    profile
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Menu
--- project/src/page/home.jsx
+++ project/src/page/home.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import Header from '../components/Header';
+import Menu from '../components/Menu';
 
 const Home = () =>{
     return(
-        <Header/>
+        <>
+            <Header/>
+            <Menu/>
+        </>
     )
 }
 
